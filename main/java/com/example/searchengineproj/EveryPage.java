7e7fe2cccdff5bed56d43873a1a1740210bdb570
Asoo <126341039+Maashooly@users.noreply.github.com>
2024-01-01 00:57:43 +0330

    every page UI

---
+++ main/java/com/example/searchengineproj/EveryPage.java
@@ -0,0 +1,15 @@
+package com.example.searchengineproj;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+public class EveryPage {
+
+    @FXML
+    private Button back;
+
+    @FXML
+    private TextArea textArea;
+
+}
---
+++ main/java/com/example/searchengineproj/SearchPage.java
@@ -0,0 +1,53 @@
+package com.example.searchengineproj;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class SearchPage {
+
+    @FXML
+    private Button back;
+
+    @FXML
+    private Button goBtn;
+
+    @FXML
+    private Hyperlink link1;
+
+    @FXML
+    private Hyperlink link2;
+
+    @FXML
+    private Hyperlink link3;
+
+    @FXML
+    private Hyperlink link4;
+
+    @FXML
+    private Hyperlink link5;
+
+    @FXML
+    private Button next;
+
+    @FXML
+    private Text t1;
+
+    @FXML
+    private Text t2;
+
+    @FXML
+    private Text t3;
+
+    @FXML
+    private Text t4;
+
+    @FXML
+    private Text t5;
+
+    @FXML
+    private TextField textField;
+
+}
